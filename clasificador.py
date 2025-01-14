85dbc5fc0f08e009bbedca80d9d85c3e820d07e1
Lucía Fernández Rodríguez <134229253+luciafhub@users.noreply.github.com>
2025-01-14 20:10:24 +0100

    Update clasificador.py

--- clasificador.py
+++ clasificador.py
@@ -119,11 +119,13 @@ def crop_with_template(image_path, template_path, output_path):
     x, y = max_loc
     h, w = template.shape
 
+    cropped_img = image[y:y+h, x:x+w]
+    
     if cropped_img is None:
         raise ValueError(f"La imagen recortada está vacía. Revisa la plantilla: {template_path}")
     cv2.imwrite(output_path, cropped_img)
 
-    cropped_img = image[y:y+h, x:x+w]
+    
     cv2.imwrite(output_path, cropped_img)
 
 # Función para analizar casillas revisando el centro
